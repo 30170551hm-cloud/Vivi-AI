070af7b77e9f32a3e0eeaf0098e86465de390fa5
Vivi AI <30170551hm@gmail.com>
2026-07-17 07:27:00 -0400

    Actualizar firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -17,4 +17,4 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-import app from './firebase';
+export { app, auth, db };
